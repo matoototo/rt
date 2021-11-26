4a1139948a09ee3c13acf0a435ae9dbd18216baf
mato <31648945+matoototo@users.noreply.github.com>
2021-11-26 14:05:40 +0100

    Add MSAA.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <functional>
 #include <limits>
 #include <memory>
+#include <random>
 
 #include "vec3.h"
 #include "ray.h"
@@ -23,22 +24,33 @@ struct Image {
     constexpr vec3 limit_x() const { return vec3(this->c.vpw, 0, 0); }
     constexpr vec3 limit_y() const { return vec3(0, this->c.vph, 0); }
 
-    void fill_pixels(std::function<color (int, int, ray&, Image&)>);
+    void fill_pixels(std::function<color (int, int, ray&, Image&)>, int);
 
     std::vector<color> img;
     const int w, h;
     Camera c;
 };
 
-void Image::fill_pixels(std::function<color (int, int, ray&, Image&)> f) {
+double rdbl(int x) {
+    if (x == 0) return 0; // lets us have a 'clean' single sample
+    static std::uniform_real_distribution<double> unif(0, 1.0);
+    static std::default_random_engine re;
+    return unif(re);
+}
+
+void Image::fill_pixels(std::function<color (int, int, ray&, Image&)> f, int aa_samples = 1) {
     auto bl = this->bottom_left();
     auto lim_x = this->limit_x();
     auto lim_y = this->limit_y();
 
     for (int j = 0; j < h; j++) {
         for (int i = 0; i < w; i++) {
-            ray r = ray(this->c.origin, bl + lim_x * double(i+1)/this->w + lim_y * double(j+1)/this->h - this->c.origin);
-            this->at(i, j) = f(i, j, r, *this);
+            color pixel = {0.0, 0.0, 0.0};
+            for (int _ = 0; _ < aa_samples; _++) {
+                ray r = ray(this->c.origin, bl + lim_x * (i+1+rdbl(_))/this->w + lim_y * (j+1+rdbl(_))/this->h - this->c.origin);
+                pixel = pixel + f(i, j, r, *this);
+            }
+            this->at(i, j) = pixel / aa_samples;
         }
     }
 }
@@ -89,7 +101,7 @@ int main() {
     Image img = Image(400, 200, cam);
 
     img.fill_pixels(create_sky);
-    img.fill_pixels(add_sphere);
+    img.fill_pixels(add_sphere, 10);
     img.to_ppm(file);
 
     return 0;
