091090ad841b1117c712e0283ae7f25d2255687e
mato <31648945+matoototo@users.noreply.github.com>
2021-11-26 15:23:32 +0100

    Move sky drawing to Scene.

--- main.cpp
+++ main.cpp
@@ -5,10 +5,6 @@
 #include "camera.h"
 #include "scene.h"
 
-color create_sky(const int& i, const int& j, const ray& r, const Image& img) {
-    double mix = r.dir.unit_vec().y()*0.5 + 0.5;
-    return (1-mix)*color(1.0, 1.0, 1.0) + mix*color(0.5, 0.7, 1.0);
-}
 
 int main() {
     std::ofstream file("test.ppm");
@@ -20,9 +16,7 @@ int main() {
     scene.add_sphere(std::make_shared<Sphere>(point3{0, -100.5, -2}, 100, color{1, 0, 0}));
     scene.add_sphere(std::make_shared<Sphere>(point3{0, 0, -4}, 1.5, color{1, 0, 0}));
 
-
-    img.fill_pixels(create_sky);
-    img.fill_pixels(scene.get_fill_func(), 1);
+    img.fill_pixels(scene.get_fill_func(), 100);
     img.to_ppm(file);
 
     return 0;
--- scene.h
+++ scene.h
@@ -18,6 +18,7 @@ struct Scene {
 
     void add_sphere(const std::shared_ptr<Sphere>&);
     color draw(const int&, const int&, const ray&, const Image&) const;
+    color draw_sky(const int&, const int&, const ray&, const Image&) const;
 
     draw_func get_fill_func() const {
         return [this](int i, int j, ray& r, Image& img) { return this->draw(i, j, r, img); };
@@ -47,5 +48,10 @@ inline color Scene::draw(const int& i, const int& j, const ray& r, const Image& 
         vec3 normal = r.at(t_last) - s_last->center;
         return 0.5*(normal.unit_vec() + 1.0);
     }
-    return img.at(i, j);
+    return draw_sky(i, j, r, img);
+}
+
+inline color Scene::draw_sky(const int& i, const int& j, const ray& r, const Image& img) const {
+    double mix = r.dir.unit_vec().y()*0.5 + 0.5;
+    return (1-mix)*color(1.0, 1.0, 1.0) + mix*color(0.5, 0.7, 1.0);
 }
